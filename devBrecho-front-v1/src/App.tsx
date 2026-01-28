da50a72f882d67724f48f34aaf5915ffa9ed8f37
LucasAPessoa <lucasapessoa@outlook.com>
2026-01-28 20:24:34 -0300

    feat: integrate AuthLayout into main routing structure

--- devBrecho-front-v1/src/App.tsx
+++ devBrecho-front-v1/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "./pages/Login/Login";
+import { AuthLayout } from "./components/layouts/auth-layout";
 // import { Setores } from "./pages/Setores";
 // import { Fornecedoras } from "./pages/Fornecedoras";
 // import { Bolsas } from "./pages/Bolsas";
@@ -7,12 +8,13 @@ import { LoginPage } from "./pages/Login/Login";
 function App() {
     return (
         <Routes>
-            <Route path="/">
+            <Route path="/" element={<AuthLayout />}>
                 <Route index element={<LoginPage />} />
                 {/* <Route path="/setores" element={<Setores />} />
                 <Route path="/fornecedoras" element={<Fornecedoras />} />
                 <Route path="/bolsas" element={<Bolsas />} /> */}
             </Route>
+            <Route path="/app"></Route>
         </Routes>
     );
 }
