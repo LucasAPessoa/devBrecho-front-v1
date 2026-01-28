e548c6bf68f853b32662be0f1358b5fa117d054d
LucasAPessoa <lucasapessoa@outlook.com>
2026-01-28 20:23:43 -0300

    feat: add index.css import to main.tsx

--- devBrecho-front-v1/src/main.tsx
+++ devBrecho-front-v1/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 
