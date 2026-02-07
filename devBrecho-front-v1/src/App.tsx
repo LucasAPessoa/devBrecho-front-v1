426f4cd0165315463351e3a8ce2ea8c6a99b42ae
LucasAPessoa <lucasapessoa@outlook.com>
2026-02-07 13:50:43 -0300

    feat: update routing structure to include AppLayout and Bags component

--- devBrecho-front-v1/src/App.tsx
+++ devBrecho-front-v1/src/App.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "./pages/Login/Login";
 import { AuthLayout } from "./components/layouts/auth-layout";
+import { AppLayout } from "./components/layouts/app-layout";
 // import { Setores } from "./pages/Setores";
 // import { Fornecedoras } from "./pages/Fornecedoras";
-// import { Bolsas } from "./pages/Bolsas";
+import { Bags } from "./pages/Bags/Bags";
 
 function App() {
     return (
@@ -11,10 +12,11 @@ function App() {
             <Route path="/" element={<AuthLayout />}>
                 <Route index element={<LoginPage />} />
                 {/* <Route path="/setores" element={<Setores />} />
-                <Route path="/fornecedoras" element={<Fornecedoras />} />
-                <Route path="/bolsas" element={<Bolsas />} /> */}
+                <Route path="/fornecedoras" element={<Fornecedoras />} />*/}
             </Route>
-            <Route path="/app"></Route>
+            <Route path="/" element={<AppLayout />}>
+                <Route path="/bolsas" element={<Bags />} />
+            </Route>
         </Routes>
     );
 }
