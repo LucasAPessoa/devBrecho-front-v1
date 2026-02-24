54d8bbf5edfe89bdc4332ee7f961d0e75bf14bf5
LucasAPessoa <lucasapessoa@outlook.com>
2026-02-24 11:24:54 -0300

    feat: add DataTable, Dialog, and Popover components with basic functionality

---
+++ devBrecho-front-v1/src/components/data-table.tsx
@@ -0,0 +1,90 @@
+import {
+    useReactTable,
+    getCoreRowModel,
+    flexRender,
+    type ColumnDef,
+} from "@tanstack/react-table";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import type { UseMutateFunction } from "@tanstack/react-query";
+import type { Bag, BagFormData } from "@/services/bagsService";
+
+interface DataTableProps<TData, TValue> {
+    data: TData[];
+    columns: ColumnDef<TData, TValue>[];
+    handleOpenModal?: () => void;
+    handleCreate?: () => UseMutateFunction<Bag, Error, BagFormData, unknown>;
+}
+
+export function DataTable<TData, TValue>({
+    data,
+    columns,
+    handleOpenModal,
+    handleCreate,
+}: DataTableProps<TData, TValue>) {
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    });
+
+    return (
+        <div className="rounded-md border">
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => {
+                                return (
+                                    <TableHead key={header.id}>
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(
+                                                  header.column.columnDef
+                                                      .header,
+                                                  header.getContext(),
+                                              )}
+                                    </TableHead>
+                                );
+                            })}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                data-state={row.getIsSelected() && "selected"}
+                            >
+                                {row.getVisibleCells().map((cell) => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(
+                                            cell.column.columnDef.cell,
+                                            cell.getContext(),
+                                        )}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell
+                                colSpan={columns.length}
+                                className="h-24 text-center"
+                            >
+                                Sem resultados, tchê.
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </div>
+    );
+}
